32a23d60325588201882c536235ece08b892b6a6
Astrid Hidalgo <hidalgo.astrid@seiza-ti.cl>
2023-10-26 21:40:58 -0300

    creacion de archivos

---
+++ MERCY-FERNANDO_HIDALGO-ASTRID/src/com/backend/parcial/dao/impl/OdontoloDaoH2.java
@@ -0,0 +1,6 @@
+package com.backend.parcial.dao.impl;
+
+public class OdontoloDaoH2 {
+
+
+}
---
+++ MERCY-FERNANDO_HIDALGO-ASTRID/src/com/backend/parcial/service/OdontologoService.java
@@ -0,0 +1,28 @@
+package com.backend.parcial.service;
+
+import com.backend.parcial.dao.IDao;
+import com.backend.parcial.model.Odontologo;
+
+import java.util.List;
+
+public class OdontologoService {
+
+        //capa de logica de negocio que interactua con la capa de persistencia, pero desconoce los detalles de la implementacion de esa persistencia
+        private IDao<Odontologo> odontologoIDaoIDao;
+
+
+        public OdontologoService(IDao<Odontologo> odontologoIDao) {
+            this.odontologoIDao = odontologoIDao;
+        }
+
+        public Odontologo registrarOdontologo(Odontologo odontologo) {
+            return odontologoIDao.registrar(odontologo);
+        }
+
+        public List<Odontologo> listarOdontologos(){
+            return odontologoIDao.listarTodos();
+        }
+
+
+
+}
